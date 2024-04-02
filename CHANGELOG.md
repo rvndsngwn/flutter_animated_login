da4f62ba76d2dde3160850836e65fdccf1c675be
arvindsangwan <rvndsngwn@gmail.com>
2024-04-02 14:48:37 +0530

    Update version and add new exports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+- add new exports
+
 ## 0.0.1
 
 - Initial release
--- lib/flutter_animated_login.dart
+++ lib/flutter_animated_login.dart
@@ -4,6 +4,8 @@ export 'src/login.dart';
 export 'src/utils/login_config.dart';
 export 'src/utils/login_data.dart';
 export 'src/utils/login_provider.dart';
+export 'src/utils/password_config.dart';
 export 'src/utils/signup_data.dart';
+export 'src/utils/verify_config.dart';
 export 'src/verify.dart';
 export 'src/widget/page.dart';
